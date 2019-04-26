0046f1bfc5b9ca156be29a5a54dcfefa3d91d571
Praful <prafulmishra0203@gmail.com>
2019-04-26 10:13:25 +0530

    initial commit

---
+++ lib/home/home_provider.dart
@@ -0,0 +1,23 @@
+import 'package:stack_scrap/home/index.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomeProvider {
+  HomeProvider();
+
+  Future<QuestionData> getData() async {
+    var res = await http
+        .get("http://192.168.31.221:8000/stack/questions/?format=json");
+    var decodedJson = jsonDecode(res.body);
+    print(decodedJson);
+    QuestionData qd = QuestionData();
+    qd.questions = [];
+
+    for (var json in decodedJson) {
+      qd.questions.add(Questions.fromJson(json));
+
+    }
+
+    return qd;
+  }
+}
